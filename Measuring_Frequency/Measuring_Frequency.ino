f83502f7acd2afea28e0b62bcc47bea2e6b38ce9
Bradley Frye <ryanfrye51@gmail.com>
2023-11-14 19:29:03 -0500

    first test

--- Measuring_Frequency/Measuring_Frequency.ino
+++ Measuring_Frequency/Measuring_Frequency.ino
@@ -1,12 +1,19 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Authors: Bradley Fyre & Nathaniel Skirvin
+// Revision 5: November 14, 2023
+// Description: Reads Red and IR signals from circuit and calculates
+//              the heart rate and SpO2 %. Then, display our results to
+//              the OLED screen.
+/////////////////////////////////////////////////////////////////////////////////
 #include <Adafruit_SSD1306.h>  // Driver for the OLED
 
 #define OLED_RESET 1         // OLED reset pin definition
 #define SCREEN_ADDRESS 0x3C  // Address for OLED
-#define CONTROL_PINA  9    // Pin 11 on MUX
-#define CONTROL_PINB  8    // Pin 10 on MUX
-#define INHIBIT       10   // Pin 6 on MUX
-#define RED      7    // Pin 12 on MUX
-#define INFRARED           6    // Pin 2 on MUX
+#define CONTROL_PINA 9       // Pin 11 on MUX
+#define CONTROL_PINB 8       // Pin 10 on MUX
+#define INHIBIT 10           // Pin 6 on MUX
+#define RED 7                // Pin 12 on MUX
+#define INFRARED 6           // Pin 2 on MUX
 // Ground goes to ay and by that goes to MUX pin 1 and 13
 // Out signal is MUX pin 14 for ax and MUX pin 15 for bx
 
@@ -16,12 +23,17 @@
 Adafruit_SSD1306 display(OLED_RESET);  // Creates a display object for OLED
 
 // Define constants related to analog signal processing
-const int analogPin = A1;           // Analog pin for input signal
-const int zeroCrossingValue = 307;  // Value corresponding to the zero-crossing voltage (1.5V in this case)
-const int buffer = 5;               // A buffer value to account for noise around the zero-crossing value
+const int ACRed = A1;           // Analog pin for input red signal
+const int ACIR = A2;            // Analog pin for IR signal
+const int DCRed = 2;
+const int DCIR = 3;
+int zeroCrossingValueRed = 102;  // Value corresponding to the zero-crossing voltage (0.5V in this case)
+int zeroCrossingValueIR = 102;   // Value corresponding to the zero-crossing voltage (0.5V in this case)
+const int buffer = 5;                  // A buffer value to account for noise around the zero-crossing value
+bool wasBelowZeroRed = true;              // Tracks last read value was below the zero-crossing
+bool wasBelowZeroIR = true;
+unsigned long lastCrossingTime = 0;    // Keeps track of the time (in microseconds) of the last detected zero-crossing
 
-bool wasBelowZero = true;            // Tracks last read value was below the zero-crossing
-unsigned long lastCrossingTime = 0;  // Keeps track of the time (in microseconds) of the last detected zero-crossing
 
 void setup() {
   pinMode(INHIBIT, OUTPUT);
@@ -46,21 +58,25 @@ void setup() {
 
 void loop() {
   digitalWrite(INHIBIT, LOW);
-  digitalWrite(CONTROL_PINA, LOW); 
-  digitalWrite(CONTROL_PINB, HIGH); 
+  digitalWrite(CONTROL_PINA, LOW);
+  digitalWrite(CONTROL_PINB, HIGH);
   digitalWrite(RED, HIGH);
   digitalWrite(INFRARED, LOW);
-  
-  int value = analogRead(analogPin);  // Reads A1
+
+  int valueRed = analogRead(ACRed);  // Reads A1
+  int valueIR = analogRead(ACIR);    // Reads A1
+  float redRMS = 0;
 
   //For debugging purposes
   Serial.print("Analog Value: ");
-  Serial.println(value);              // Print the read analog value to the serial monitor
+  Serial.println(valueRed);  // Print the read analog value to the serial monitor
 
   // Check if a positive-going zero-crossing is detected
-  if (wasBelowZero && value >= (zeroCrossingValue + buffer)) {
-    unsigned long currentCrossingTime = micros(); // Get the current timestamp
-    unsigned long period = currentCrossingTime - lastCrossingTime; // Calculate the time difference since the last zero-crossing
+  // if (wasBelowZero && value >= (zeroCrossingValue + buffer)) {
+  if (wasBelowZeroRed) {
+    redRMS = calculateRMS(zeroCrossingValueRed, lastCrossingTime);
+    unsigned long currentCrossingTime = micros();                   // Get the current timestamp
+    unsigned long period = currentCrossingTime - lastCrossingTime;  // Calculate the time difference since the last zero-crossing
 
     // Only calculate the frequency if the period is valid (greater than zero)
     if (period > 0) {
@@ -73,27 +89,29 @@ void loop() {
       Serial.print("BPM: ");
       Serial.println(bpm);
 
-      displayFrequencyAndBPM(frequency, bpm); // Update the OLED display with the new frequency and BPM values
+      //displayFrequencyAndBPM(frequency, bpm);  // Update the OLED display with the new frequency and BPM values
 
-      lastCrossingTime = currentCrossingTime; // Update the timestamp for the next zero-crossing detection
+      lastCrossingTime = currentCrossingTime;  // Update the timestamp for the next zero-crossing detection
     }
-    wasBelowZero = false;  // Reset for next cycle
+    wasBelowZeroRed = false;  // Reset for next cycle
   }
   // Check if the waveform has gone below the zero-crossing minus buffer
-  else if (value < (zeroCrossingValue - buffer)) {
-    wasBelowZero = true;   // Signal is now below the zero-crossing, triggers if statement above to do its calculations
+  else if (valueRed < (zeroCrossingValueRed - buffer)) {
+    wasBelowZeroRed = true;  // Signal is now below the zero-crossing, triggers if statement above to do its calculations
   }
-  delay(2); // Waits 500 Hz, 2 ms
+  delay(2);  // Waits 500 Hz, 2 ms
 
   digitalWrite(INHIBIT, LOW);
-  digitalWrite(CONTROL_PINA, HIGH);  
-  digitalWrite(CONTROL_PINB, LOW);  
+  digitalWrite(CONTROL_PINA, HIGH);
+  digitalWrite(CONTROL_PINB, LOW);
   digitalWrite(RED, LOW);
   digitalWrite(INFRARED, HIGH);
-  
-   if (wasBelowZero && value >= (zeroCrossingValue + buffer)) {
-    unsigned long currentCrossingTime = micros(); // Get the current timestamp
-    unsigned long period = currentCrossingTime - lastCrossingTime; // Calculate the time difference since the last zero-crossing
+
+  if (wasBelowZeroIR) {
+    float IRRMS = calculateRMS(zeroCrossingValueRed, lastCrossingTime);
+    float SpO2 = calculateSpO2(DCRed, DCIR, redRMS, IRRMS);
+    unsigned long currentCrossingTime = micros();                   // Get the current timestamp
+    unsigned long period = currentCrossingTime - lastCrossingTime;  // Calculate the time difference since the last zero-crossing
 
     // Only calculate the frequency if the period is valid (greater than zero)
     if (period > 0) {
@@ -106,19 +124,19 @@ void loop() {
       Serial.print("BPM: ");
       Serial.println(bpm);
 
-      displayFrequencyAndBPM(frequency, bpm); // Update the OLED display with the new frequency and BPM values
+      displayFrequencyAndBPM(frequency, bpm, SpO2);  // Update the OLED display with the new frequency and BPM values
 
-      lastCrossingTime = currentCrossingTime; // Update the timestamp for the next zero-crossing detection
+      lastCrossingTime = currentCrossingTime;  // Update the timestamp for the next zero-crossing detection
     }
-    wasBelowZero = false;  // Reset for next cycle
+    wasBelowZeroIR = false;  // Reset for next cycle
   }
   // Check if the waveform has gone below the zero-crossing minus buffer
-  else if (value < (zeroCrossingValue - buffer)) {
-    wasBelowZero = true;   // Signal is now below the zero-crossing, triggers if statement above to do its calculations
+  else if (valueIR < (zeroCrossingValueIR - buffer)) {
+    wasBelowZeroIR = true;  // Signal is now below the zero-crossing, triggers if statement above to do its calculations
   }
-  delay(2); // Waits 500 Hz, 2ms
+  delay(2);  // Waits 500 Hz, 2ms
 }
-//Nathan gay 
+
 // Function to display title screen
 void displayTitleScreen() {
   display.clearDisplay();
@@ -141,7 +159,7 @@ void displayTitleScreen() {
 }
 
 // Function to display the frequency and BPM values on the OLED screen
-void displayFrequencyAndBPM(float freq, float bpm) {
+void displayFrequencyAndBPM(float freq, float bpm, float SpO2) {
   display.clearDisplay();       // Clear the display
   display.setTextSize(1);       // Set text size
   display.setTextColor(WHITE);  // Set text color
@@ -166,7 +184,22 @@ void displayFrequencyAndBPM(float freq, float bpm) {
   //Dislay SpO2 %
   display.setCursor(0, 24);
   display.print("SpO2: ");
+  display.print(SpO2);
   display.println(" %");
 
   display.display();  // Refresh the OLED screen to show the updated values
+}
+
+// Calculate the VRMS 
+float calculateRMS(int highestCrossing, int lowestCrossing) {
+  double Avg = (highestCrossing - lowestCrossing) / 2;
+  float RMS = Avg * (1/sqrt(2));
+
+  return RMS;
+}
+
+// Calculate SpO2 %
+float calculateSpO2(int DCRed, int DCIR, float redRMS, float IRRMS) {
+  float SpO2 = (IRRMS / DCIR) / (redRMS / DCRed);
+  return SpO2; 
 }
